41268a74fc7825e48930ae632c8143f8a4f040c4
toanduong <toanduong@bbv.ch>
2019-11-14 15:27:19 +0700

    update some changes

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,16 +1,12 @@
 
-console.log('Implement power-up');
+console.log('Check card-back-section');
 
 window.TrelloPowerUp.initialize({
     'card-back-section': function(t, options){
       return {
         title: 'My Card Back Section',
         icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
-        content: {
-          type: 'iframe',
-          url: t.signUrl('./section.html'),
-          height: 230 // Max height is 500
-        }
+        content: "test my card"
       };
     }
   });
