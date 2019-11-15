25d682fb4aff7c30a928b6a34e090a37fe4ea0c0
toanduong <toanduong@bbv.ch>
2019-11-15 13:43:53 +0700

    update some changes

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,26 +5,24 @@ var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4b
 
 window.TrelloPowerUp.initialize({
     'card-back-section': function(t, options){
-        var technology = "FIBER";
-        console.log(options.context);
-        $.ajax({
-            method: "GET",
-            url: "http://umbrellatooltest.intern.upc.ch/Connector/GetCustomFields",
-            data: {
-                technology: technology
-            },
-            success: function (e) {
-                console.log(e);
-            }
-        });
-      return {
-        title: 'Custom fields',    
-        icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.    
-        content: {
-            type: 'iframe',
-            url: t.signUrl('./section.html'),
-            height: 230 // Max height is 500
-          }
-      };
+        return t.set(options.context.card, 'shared', '123456789', '123456789')
+                .then(function(){
+                    //call api to get card information in trello app
+                    $.ajax({
+                        url: 'https://api.trello.com/1/cards/' + options.context.card + '?key=cc57856e5af7a9464cdd18d4392623c2&token=fb8213d20972a81b10b9df437e19e99bfdc71bbad832493c14ab753b385e281b',
+                        success: function(e) {
+                            console.log(e);
+                        }
+                    });
+                });
+    //   return {
+    //     title: 'Custom fields',    
+    //     icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.    
+    //     content: {
+    //         type: 'iframe',
+    //         url: t.signUrl('./section.html'),
+    //         height: 230 // Max height is 500
+    //       }
+    //   };
     }
   });
