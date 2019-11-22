cca195000a31585433c2147f706d09d2d5c5ec4e
toanduong <toanduong@bbv.ch>
2019-11-22 08:48:58 +0700

    update some changes

--- src/js/connector.js
+++ src/js/connector.js
@@ -48,7 +48,7 @@ window.TrelloPowerUp.initialize(
                                     $.each(customField.options, function(j, option) {
                                         if(option.id == selectedValue[customField.id]) {
                                             values.push({
-                                                icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+                                                icon: './setting.png',
                                                 text: option.value,
                                                 color: option.color
                                             });
