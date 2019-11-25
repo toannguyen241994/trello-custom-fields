5eb6897a08163326a5cc60d2ff9543bcb3f9eb1f
toanduong <toanduong@bbv.ch>
2019-11-25 11:33:19 +0700

    update some changes

--- src/js/connector.js
+++ src/js/connector.js
@@ -12,15 +12,11 @@ window.TrelloPowerUp.initialize(
                         $.each(data, function(i, card) {
                             var desc = card.desc;
                             if (desc.indexOf('§FIELD_SELECTED=') > -1) {
-                                console.log('' + desc.indexOf('§FIELD_SELECTED='));
                                 var selectedValue = desc.substring(desc.indexOf('§FIELD_SELECTED='), desc.length).replace('§FIELD_SELECTED=', '').replace('§', '');
-                                console.log(selectedValue);
                                 var selectedCustom = JSON.parse(selectedValue);
-                                console.log(selectedCustom);
                                 return t.set(card.id, 'shared', card.id + '-selected', selectedCustom)
                                     .then(function(){
                                         desc = desc.substring(0, desc.indexOf('§FIELD_SELECTED='));
-                                        console.log(desc);
                                         $.ajax({
                                             url: 'https://api.trello.com/1/cards/' + card.id + '?' + KEYTOKEN,
                                             type: 'PUT',
@@ -28,31 +24,49 @@ window.TrelloPowerUp.initialize(
                                                 desc: desc
                                             },
                                             success: function(e){
-                                                
+                                                if (desc.indexOf('§FIELDS=') > -1) {
+                                                    var length = desc.indexOf('§FIELD_SELECTED=') > -1 ? desc.indexOf('§FIELD_SELECTED=') : desc.length;
+                                                    var value = desc.substring(desc.indexOf('§FIELDS='), length).replace('§FIELDS=', '').replace('§', '');
+                                                    var customFields = JSON.parse(value);
+                                                    return t.set(card.id, 'shared', card.id, customFields)
+                                                        .then(function(){
+                                                            desc = desc.substring(0, desc.indexOf('§FIELDS='));
+                                                            $.ajax({
+                                                                url: 'https://api.trello.com/1/cards/' + card.id + '?' + KEYTOKEN,
+                                                                type: 'PUT',
+                                                                data: {
+                                                                    desc: desc
+                                                                },
+                                                                success: function(e){
+                                                                    
+                                                                }
+                                                            });
+                                                        });
+                                                }
                                             }
                                         });
                                     });
-                            }
-                            if (desc.indexOf('§FIELDS=') > -1) {
-                                var length = desc.indexOf('§FIELD_SELECTED=') > -1 ? desc.indexOf('§FIELD_SELECTED=') : desc.length;
-                                var value = desc.substring(desc.indexOf('§FIELDS='), length).replace('§FIELDS=', '').replace('§', '');
-                                var customFields = JSON.parse(value);
-                                return t.set(card.id, 'shared', card.id, customFields)
-                                    .then(function(){
-                                        desc = desc.substring(0, desc.indexOf('§FIELDS='));
-                                        $.ajax({
-                                            url: 'https://api.trello.com/1/cards/' + card.id + '?' + KEYTOKEN,
-                                            type: 'PUT',
-                                            data: {
-                                                desc: desc
-                                            },
-                                            success: function(e){
-                                                
-                                            }
+                            } else {
+                                if (desc.indexOf('§FIELDS=') > -1) {
+                                    var length = desc.length;
+                                    var value = desc.substring(desc.indexOf('§FIELDS='), length).replace('§FIELDS=', '').replace('§', '');
+                                    var customFields = JSON.parse(value);
+                                    return t.set(card.id, 'shared', card.id, customFields)
+                                        .then(function(){
+                                            desc = desc.substring(0, desc.indexOf('§FIELDS='));
+                                            $.ajax({
+                                                url: 'https://api.trello.com/1/cards/' + card.id + '?' + KEYTOKEN,
+                                                type: 'PUT',
+                                                data: {
+                                                    desc: desc
+                                                },
+                                                success: function(e){
+                                                    
+                                                }
+                                            });
                                         });
-                                    });
+                                }
                             }
-                            
                         });
                         
                     });
